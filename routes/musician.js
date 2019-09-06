224bb16ebb3b2d1706143f851e6912dc467d86c6
pradeepch82 <pradeepch82@gmail.com>
2019-09-06 20:16:52 +0530

    commit 2
    added new route hi

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,11 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+router.get('/hi', (req, res) => {
+  res.status('200').send("Hi all.......................");
+});
+
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
