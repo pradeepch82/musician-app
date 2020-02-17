99fa262d6b3593eda97c3fddcc8c83aca8f839aa
Pradeep Chinchole <pradeepch82@gmail.com>
2020-02-17 16:25:15 +0530

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Hello Pradeep Status: ok!");
+  res.status('200').send("Hello Rejendra Status: ok!");
 });
 
 router.get('/music', (req, res) => {
